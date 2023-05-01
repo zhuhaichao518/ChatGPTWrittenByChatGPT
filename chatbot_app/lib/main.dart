923eb7089f9f6937f0e106bb417ff3b099abc929
zhuhaichao518 <zhuhaichao518@gmail.com>
2023-05-02 00:28:58 +0800

    use stream instead

--- chatbot_app/lib/main.dart
+++ chatbot_app/lib/main.dart
@@ -1,9 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/io_client.dart' as http_io;
-import 'dart:math' as math;
 
 void main() {
   runApp(const ChatbotApp());
@@ -39,64 +39,65 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = TextEditingController();
   final List<String> _messages = [];
-  final List<Map<String, String>> _conversation = [
-  ];
+  final List<Map<String, String>> _conversation = [];
   bool _isButtonDisabled = false;
 
-Widget _buildMessageList() {
-  return ListView.builder(
-    itemCount: _messages.length,
-    itemBuilder: (context, index) {
-      bool isUserMessage = index % 2 == 0;
-      return Column(
-        children: [
-              Row(
-                mainAxisAlignment: isUserMessage
-                    ? MainAxisAlignment.end
-                    : MainAxisAlignment.start,
-                children: [
-                  if (!isUserMessage) ...[
-                    CircleAvatar(
-                      backgroundImage: AssetImage('assets/images/chatbot_avatar.png'),
+  Widget _buildMessageList() {
+    return ListView.builder(
+      itemCount: _messages.length,
+      itemBuilder: (context, index) {
+        bool isUserMessage = index % 2 == 0;
+        return Column(
+          children: [
+            Row(
+              mainAxisAlignment: isUserMessage
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
+              children: [
+                if (!isUserMessage) ...[
+                  CircleAvatar(
+                    backgroundImage:
+                        AssetImage('assets/images/chatbot_avatar.png'),
+                  ),
+                  SizedBox(width: 8.0),
+                ],
+                CustomPaint(
+                  painter: BubblePainter(isUser: isUserMessage),
+                  child: Container(
+                    constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width * 0.6,
                     ),
-                    SizedBox(width: 8.0),
-                  ],
-                  CustomPaint(
-                    painter: BubblePainter(isUser: isUserMessage),
-                    child: Container(
-                      constraints: BoxConstraints(
-                        maxWidth: MediaQuery.of(context).size.width * 0.6,
-                      ),
-                      margin: EdgeInsets.fromLTRB(
-                        isUserMessage ? 16.0 : 16.0,
-                        4.0,
-                        isUserMessage ? 16.0 : 16.0,
-                        4.0,
-                      ),
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        _messages[index],
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    margin: EdgeInsets.fromLTRB(
+                      isUserMessage ? 16.0 : 16.0,
+                      4.0,
+                      isUserMessage ? 16.0 : 16.0,
+                      4.0,
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: SelectableText(
+                      _messages[index],
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                  if (isUserMessage) ...[
-                    SizedBox(width: 8.0),
-                    CircleAvatar(
-                      backgroundImage: AssetImage('assets/images/user_avatar.png'),
-                    ),
-                  ],
+                ),
+                if (isUserMessage) ...[
+                  SizedBox(width: 8.0),
+                  CircleAvatar(
+                    backgroundImage:
+                        AssetImage('assets/images/user_avatar.png'),
+                  ),
                 ],
-              ),
-              SizedBox(height: 8.0),
-        ],
-      );
-    },
-  );
-}
+              ],
+            ),
+            SizedBox(height: 8.0),
+          ],
+        );
+      },
+    );
+  }
 
   Future<void> sendMessage(String message) async {
     setState(() {
@@ -113,31 +114,44 @@ Widget _buildMessageList() {
     };
 
     Map<String, dynamic> body = {
-      'model': 'gpt-4',//'gpt-3.5-turbo',
+      'model': 'gpt-4', //'gpt-3.5-turbo',
       'messages': _conversation,
-      "temperature": 0.7,
+      'temperature': 0.7,
+      'stream': true,
     };
 
     try {
       http.Client httpClient = createHttpClient();
-      final response = await httpClient.post(
-        Uri.parse(apiUrl),
-        headers: headers,
-        body: json.encode(body),
-      );
+      final request = http.Request('POST', Uri.parse(apiUrl));
+      request.headers.addAll(headers);
+      request.body = json.encode(body);
+      final streamedResponse = await httpClient.send(request);
 
-      if (response.statusCode == 200) {
-        final responseData = json.decode(utf8.decode(response.bodyBytes));
-        final String reply = responseData['choices'][0]['message']['content'].trim();
-
-        setState(() {
-          _conversation.add({'role': 'assistant', 'content': reply});
-          _messages.add('$reply');
-          _isButtonDisabled = false;
-        });
-      } else {
-        throw Exception(response.reasonPhrase);
+      final linesStream = streamedResponse.stream
+          .asBroadcastStream()
+          .transform(utf8.decoder)
+          .transform(const LineSplitter());
+      
+      _messages.add('');
+      int myid = _messages.length -1;
+      await for (String line in linesStream) {
+        if (line != '' && line!= 'data: [DONE]') {
+          String jsonData = line.substring(6);
+          Map<String, dynamic> decodedJson = jsonDecode(jsonData);
+          if (decodedJson['choices'][0]['delta'].containsKey('content')) {
+            String deltaContent = decodedJson['choices'][0]['delta']['content'];
+            print('Delta content: $deltaContent');
+            setState(() {
+              _messages[myid] = _messages[myid] + deltaContent;
+              _isButtonDisabled = false;
+            });
+          }
+        }
       }
+      setState(() {
+          _conversation.add({'role': 'assistant', 'content': _messages.last});
+          _isButtonDisabled = false;
+      });
     } catch (error) {
       setState(() {
         _messages.add('$error');
@@ -146,60 +160,63 @@ Widget _buildMessageList() {
     }
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('ChatGPT Chatbot'),
-      backgroundColor: Colors.blue.shade900,
-    ),
-    body: Column(
-      children: [
-        Expanded(child: _buildMessageList()),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Row(
-            children: [
-              Flexible( // 新增的 Flexible 部件
-                child: TextField(
-                  controller: _textController,
-                  decoration: InputDecoration(
-                    hintText: 'Type your message',
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ChatGPT Chatbot'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Column(
+        children: [
+          Expanded(child: _buildMessageList()),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: [
+                Flexible(
+                  // 新增的 Flexible 部件
+                  child: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      hintText: 'Type your message',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(width: 8.0),
-              ElevatedButton(
-                onPressed: _isButtonDisabled ? null:()
-                {
-                  final String message = _textController.text.trim();
-                  if (message.isNotEmpty) {
-                    sendMessage(message);
-                    _textController.clear();
-                  }
-                },
-                child: const Text('Send'),
-                style: ElevatedButton.styleFrom(
-                  primary: _isButtonDisabled ? Colors.grey : Colors.blue.shade900,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                SizedBox(width: 8.0),
+                ElevatedButton(
+                  onPressed: _isButtonDisabled
+                      ? null
+                      : () {
+                          final String message = _textController.text.trim();
+                          if (message.isNotEmpty) {
+                            sendMessage(message);
+                            _textController.clear();
+                          }
+                        },
+                  child: const Text('Send'),
+                  style: ElevatedButton.styleFrom(
+                    primary:
+                        _isButtonDisabled ? Colors.grey : Colors.blue.shade900,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    minimumSize: Size(88, 58),
                   ),
-                  minimumSize: Size(88, 58),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        SizedBox(height: 10.0),
-      ],
-    ),
-  );
-}
+          SizedBox(height: 10.0),
+        ],
+      ),
+    );
+  }
 }
 
 class BubblePainter extends CustomPainter {
@@ -241,7 +258,7 @@ class BubblePainter extends CustomPainter {
     path.lineTo(borderRadius, size.height);
     path.arcToPoint(Offset(0, size.height - borderRadius),
         radius: Radius.circular(borderRadius));
-    
+
     if (!isUser) {
       path.lineTo(0, mid + arrowHeight);
       path.lineTo(0 - arrowWidth, mid);
